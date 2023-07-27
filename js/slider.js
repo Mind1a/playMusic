0c965b1135366372109a27ef7d9128f0ead135de
Lipartiaana <124500573+Lipartiaana@users.noreply.github.com>
2023-07-27 21:26:10 +0400

    feature/sliderUpdate
    Updating arrow styles and adding function that generates images from js.

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,19 @@
+import sliderImagesArray from "./data.js";
+
+// Generates images
+
+sliderImagesArray.forEach((image) => {
+  const slider = document.querySelector(".swiper-wrapper");
+  const swiperSlide = document.createElement("div");
+  swiperSlide.classList.add("swiper-slide");
+  const sliderHTML = `<img src="${image}" alt="slider-image" />`;
+
+  swiperSlide.innerHTML = sliderHTML;
+  slider.appendChild(swiperSlide);
+});
+
+// Creates slider
+
 const swiper = new Swiper(".slider", {
   navigation: {
     nextEl: ".swiper-button-next",
